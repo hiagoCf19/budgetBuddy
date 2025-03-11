ab42ceb372bb43d2cc828ecdfb0750ae5c053b2c
hiagoCf19 <hiagoferreira@id.uff.br>
2025-03-11 19:47:58 -0300

    setup:infra

---
+++ src/main/java/com/BudgetBuddy/demo/application/dto/User.java
@@ -0,0 +1,4 @@
+package com.BudgetBuddy.demo.application.dto;
+
+public record User(String user, String password, String clientId, String grantType) {
+}
---
+++ src/main/java/com/BudgetBuddy/demo/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.BudgetBuddy.demo.infrastructure.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain secutiryFilterChain(HttpSecurity http) throws Exception{
+        return http.build();
+    }
+}
